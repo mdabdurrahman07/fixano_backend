33bba76b636fd7aa220d1d09a360a45093eaca98
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-07-06 20:59:15 +0600

    feat: env config updated and necessary dependencis installed

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express, { Application, Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import config from './config/config.dotenv';
 
 const app: Application = express();
 
 // express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true
+  })
+);
 
 // root
 
--- src/config/config.dotenv.ts
+++ src/config/config.dotenv.ts
@@ -10,6 +10,15 @@ const config = {
   port: env.PORT as string,
   database_url: env.DATABASE_URL as string,
   node_env: env.NODE_ENV as string,
+  app_url: env.APP_URL as string,
+  bcrypt_salt_rounds: env.BCRYPT_SALT_ROUNDS as string,
+  jwt_access_secret: env.JWT_ACCESS_SECRET as string,
+  jwt_refresh_secret: env.JWT_REFRESH_SECRET as string,
+  jwt_access_expires_in: env.JWT_ACCESS_EXPIRES_IN as string,
+  jwt_refresh_expires_in: env.JWT_REFRESH_EXPIRES_IN as string,
+  stripe_secret_key: env.STRIPE_SECRET_KEY as string,
+  stripe_price_id: env.STRIPE_PRICE_ID as string,
+  stripe_webhook_secret_key: env.STRIPE_WEBHOOK_SECRET_KEY as string
 };
 
 export default config;
