dfd50511223ec923bd81ea3ae63c08e9c4d0e29b
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-07-06 20:45:24 +0600

    feat: prisma implemented and initail setup implemented and DB is connected

--- src/config/config.dotenv.ts
+++ src/config/config.dotenv.ts
@@ -7,6 +7,7 @@ dotenv.config({
 });
 
 const config = {
+  port: env.PORT as string,
   database_url: env.DATABASE_URL as string,
   node_env: env.NODE_ENV as string,
 };
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import app from './app';
 import config from './config/config.dotenv';
+import { prisma } from './lib/prisma';
 
 if (config.node_env !== 'production') {
   async function server() {
-    const port = 8080;
+    const port = config.port;
     try {
+      await prisma.$connect();
+      console.log('DB is connected');
       app.listen(port, () => {
         console.log(`Fixano backend server is running on ${port}`);
       });
     } catch (error) {
       console.log('Error starting the server', error);
+      await prisma.$disconnect();
+      process.exit(1);
     }
   }
   server();
