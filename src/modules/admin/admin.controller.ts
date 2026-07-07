7c75caa90e183728478a121b06702fb2a977ec8a
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-07-07 15:19:36 +0600

    feat: admin section getAllUser and updateUserStatus feature implemented

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,9 +1,30 @@
+import httpStatus from 'http-status';
 import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../../util/catchAsync';
+import { adminService } from './admin.service';
+import { sendResponse } from '../../util/sendResponse';
 
-const getAllUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
+const getAllUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const result = await adminService.fetchAllUsers();
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'All Customer data retrieved successfully',
+    data: result
+  });
+});
 
-const patchUserStatus = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
+const patchUserStatus = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const payload = req.body;
+  const id = req.params.id as string
+  const result = await adminService.updateUserStatus(payload, id);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Customer status active updated',
+    data: result
+  });
+});
 
 const getAllBookings = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
 
--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -1,5 +1,45 @@
-const fetchAllUsers = async () => {};
-const updateUserStatus = async () => {};
+import { UserActiveStatus } from '../../../generated/prisma/enums';
+import { prisma } from '../../lib/prisma';
+
+const fetchAllUsers = async () => {
+  const allUser = await prisma.user.findMany({
+    omit: {
+      password: true
+    },
+    include: {
+      technician: true
+    }
+  });
+  return allUser;
+};
+const updateUserStatus = async ({status}: {status: UserActiveStatus}, userId: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId
+    }
+  });
+  if (!user) {
+    throw new Error('User not found by this id');
+  }
+
+  if (user.status === status) {
+    throw new Error(`User status is already ${status}. No database update needed`);
+  }
+
+  const updateStatus = await prisma.user.update({
+    where: { id: userId },
+    data: {
+      status: status
+    },
+    omit: {
+      password: true
+    },
+    include: {
+      technician: true
+    }
+  });
+  return updateStatus;
+};
 const fetchAllBookings = async () => {};
 const fetchAllCategories = async () => {};
 const createNewCategory = async () => {};
