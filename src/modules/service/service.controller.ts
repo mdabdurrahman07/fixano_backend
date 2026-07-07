954670da0e368d425c56fb507b532d78e7c1a8a9
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-07-07 22:46:37 +0600

    feat: service(public) router controller and service implemented but filtering work-in-progress

--- src/modules/service/service.controller.ts
+++ src/modules/service/service.controller.ts
@@ -1,13 +1,49 @@
+import  httpStatus  from 'http-status';
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../util/catchAsync";
+import { serviceService } from "./service.service";
+import { sendResponse } from "../../util/sendResponse";
 
-const getAllServices = catchAsync(async (req: Request, res: Response, next: NextFunction) => {})
+const getAllServices = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const result = await serviceService.fetchAllServices()
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "All services retrieved successfully",
+        data: result
+    })
+})
 
-const getAllTechnicians = catchAsync(async (req: Request, res: Response, next: NextFunction) => {})
+const getAllTechnicians = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const result = await serviceService.fetchAllTechnicians()
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "All technician retrieved successfully",
+        data: result
+    })
+})
 
-const getSingleTechnician = catchAsync(async (req: Request, res: Response, next: NextFunction) => {})
+const getSingleTechnician = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const technicianId = req.params.id as string
+    const result = await serviceService.fetchSingleTechnician(technicianId)
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "Single technician retrieved successfully",
+        data: result
+    })
+})
 
-const getAllCategories = catchAsync(async (req: Request, res: Response, next: NextFunction) => {})
+const getAllCategories = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const result = await serviceService.fetchAllCategories()
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "All categories retrieved successfully",
+        data: result
+    })
+})
 
 export const serviceController = {
     getAllServices,
--- src/modules/service/service.service.ts
+++ src/modules/service/service.service.ts
@@ -1,7 +1,35 @@
-const fetchAllServices = async () => {};
-const fetchAllTechnicians = async () => {};
-const fetchSingleTechnician = async () => {};
-const fetchAllCategories = async () => {};
+import { prisma } from '../../lib/prisma';
+
+const fetchAllServices = async () => {
+  const services = await prisma.service.findMany();
+  return services;
+};
+const fetchAllTechnicians = async () => {
+  const technicians = await prisma.technician.findMany();
+  return technicians;
+};
+const fetchSingleTechnician = async (technicianId: string) => {
+  const technician = await prisma.technician.findUnique({
+    where: {
+      id: technicianId
+    },
+    include:{
+      reviews: true
+    }
+  });
+  if (!technician) {
+    throw new Error('No technician found by this id');
+  }
+  return technician;
+};
+const fetchAllCategories = async () => {
+  const categories = await prisma.category.findMany({
+    include:{
+      services: true
+    }
+  })
+  return categories
+};
 
 export const serviceService = {
   fetchAllServices,
