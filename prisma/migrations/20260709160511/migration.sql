1153960e6353661de83653768228fccce35e4cc3
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-07-13 22:17:10 +0600

    migration and package.json file added

---
+++ prisma/migrations/20260709160511/migration.sql
@@ -0,0 +1,46 @@
+/*
+  Warnings:
+
+  - You are about to drop the `payment` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "payment" DROP CONSTRAINT "payment_bookingId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "payment" DROP CONSTRAINT "payment_customerId_fkey";
+
+-- DropTable
+DROP TABLE "payment";
+
+-- CreateTable
+CREATE TABLE "payments" (
+    "id" TEXT NOT NULL,
+    "amount" DECIMAL(10,2) NOT NULL,
+    "provider" TEXT NOT NULL DEFAULT 'Stripe',
+    "status" "PaymentStatus" NOT NULL DEFAULT 'PENDING',
+    "stripePaymentIntentId" TEXT NOT NULL,
+    "stripeSessionId" TEXT NOT NULL,
+    "paidAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "bookingId" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+
+    CONSTRAINT "payments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payments_stripePaymentIntentId_key" ON "payments"("stripePaymentIntentId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payments_stripeSessionId_key" ON "payments"("stripeSessionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payments_bookingId_key" ON "payments"("bookingId");
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_bookingId_fkey" FOREIGN KEY ("bookingId") REFERENCES "bookings"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
